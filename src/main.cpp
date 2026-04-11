a1a129137b7bfd333a6d9f921c8acdbcd71a0da6
kane baltazar alanoca <developer.innovaappstar@gmail.com>
2026-04-10 23:22:57 -0500

    simple button done

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 
 #define BUTTON_PIN 15 // D15
+#define LED_PIN 2
 
 int lastState = HIGH;
 void setup()
@@ -10,6 +11,7 @@ void setup()
 
   // pinMode(BUTTON_PIN, INPUT); // pull-down EXTERNO
   pinMode(BUTTON_PIN, INPUT_PULLUP); // pull-down EXTERNO
+  pinMode(LED_PIN, OUTPUT);
 
   Serial.println("ESP32 listo. Presiona el botón (GPIO 15).");
 }
@@ -18,16 +20,17 @@ void loop()
 {
   int currentState = digitalRead(BUTTON_PIN);
 
-
   if (currentState != lastState)
   {
     if (currentState == LOW)
-    { 
+    {
       Serial.println("[BOTON] PRESIONADO");
+      digitalWrite(LED_PIN, HIGH);
     }
     else
     {
       Serial.println("[BOTON] LIBERADO");
+      digitalWrite(LED_PIN, LOW);
     }
     delay(50);
   }
@@ -35,5 +38,4 @@ void loop()
   lastState = currentState;
 }
 
-// OPCIÓN 1: Pull-down externo (como tu código actual)
-// OPCIÓN 2 (MEJOR): usar pull-up interno del ESP32
+// pull down externo vs pull up interno
